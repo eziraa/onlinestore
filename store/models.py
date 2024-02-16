af1b8496d3e0eaa965cf1ee213e09953a24cfdbd
eziraa <ezratgab@gmail.com>
2024-02-17 00:50:22 +0300

    create many to many relationship between Order and Product through OrderItem class

--- store/models.py
+++ store/models.py
@@ -34,8 +34,6 @@ class Customer(models.Model):
         max_length=1, choices=MEMBESHIP_CHOICES, default=MEMBERSHIP_BRONZE)
 
 # Create Order Class
-
-
 class Order(models.Model):
     PAYMENT_STATUS_PENDING = 'P'
     PAYMENT_STATUS_COMPLETED = 'C'
@@ -51,3 +49,10 @@ class Order(models.Model):
     pyment_status = models.CharField(
         max_length=1, choices=PAYMENT_STATUS_CHOICES, default=PAYMENT_STATUS_PENDING)
     customer = models.ForeignKey(Customer, on_delete=models.PROTECT)
+
+
+class OrderItem (models.Model):
+    order = models.ForeignKey(Order, on_delete=models.PROTECT)
+    product = models.ForeignKey(Product, on_delete=models.PROTECT)
+    quantity = models.PositiveSmallIntegerField()
+    unit_price = models.DecimalField(max_digits=6, decimal_places=2)
