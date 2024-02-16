aad40e159cb828b37414db5117e7670fa9b9c067
eziraa <ezratgab@gmail.com>
2024-02-16 20:48:02 +0300

    creating store app and adding to installed app

--- onlinestore/settings.py
+++ onlinestore/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'playground',
+    'store',
 ]
 
 MIDDLEWARE = [
--- playground/views.py
+++ playground/views.py
@@ -3,4 +3,4 @@ from django.http import HttpResponse
 
 
 def index(request):
-    return render(request, 'index.html')
+    return render(request, 'index.html', {'name': 'Ezira'})
