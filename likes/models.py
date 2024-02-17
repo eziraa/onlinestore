2a6d20dccf99f7772a24950a916f6489e6601948
eziraa <ezratgab@gmail.com>
2024-02-17 10:10:22 +0300

    adding generic relationship for what user likes waht obejct

--- likes/models.py
+++ likes/models.py
@@ -1,1 +1,12 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.contrib.contenttypes.models import ContentType
+from django.contrib.contenttypes.fields import GenericForeignKey
+# create LikedItem model to represent what user likes what product
+
+
+class LikedItem(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    content_type = models.ForeignKey(ContentType, on_delete=models.CASCADE)
+    object_id = models.PositiveSmallIntegerField()
+    content_object = GenericForeignKey()
