4195be4fa794b24fed14868a1f2904cfcaecae72
eziraa <ezratgab@gmail.com>
2024-02-17 10:00:54 +0300

    create TaggedItem model to make generic realtionship between Tag and product

--- tags/models.py
+++ tags/models.py
@@ -1,7 +1,17 @@
 from django.db import models
-
+from django.contrib.contenttypes.models import ContentType
+from django.contrib.contenttypes.fields import GenericForeignKey
 # create tag model
 
 
 class Tag(models.Model):
     label = models.CharField(max_length=255)
+
+# create TeggedItem model
+
+
+class TaggeItem(models.Model):
+    tag = models.ForeignKey(Tag, on_delete=models.CASCADE)
+    content_type = models.ForeignKey(ContentType, on_delete=models.CASCADE)
+    object_id = models.PositiveSmallIntegerField()
+    content_object = GenericForeignKey()
