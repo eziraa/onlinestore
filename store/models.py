27743c4aa902937a817738b1b57fa0799965ba90
eziraa <ezratgab@gmail.com>
2024-02-17 11:22:05 +0300

    adding slug field and makemigration in product model

--- store/models.py
+++ store/models.py
@@ -16,6 +16,7 @@ class Collection(models.Model):
 class Product(models.Model):
     title = models.CharField(max_length=255)
     description = models.TextField()
+    slug = models.SlugField(default='-')
     unit_price = models.DecimalField(max_digits=6, decimal_places=2)
     inventory = models.IntegerField()
     last_update = models.DateTimeField(auto_now=True)
