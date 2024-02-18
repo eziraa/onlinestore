a99af242645b4d982ebc118144781a9967662266
eziraa <ezratgab@gmail.com>
2024-02-19 00:59:17 +0300

    registering models of store app

--- store/admin.py
+++ store/admin.py
@@ -1,3 +1,4 @@
 from django.contrib import admin
-
-# Register your models here.
+from .models import Collection, Order, OrderItem, Customer, Product, Promotion
+list_of_models = [Collection, Order, OrderItem, Customer, Product, Promotion]
+admin.site.register(list_of_models)
