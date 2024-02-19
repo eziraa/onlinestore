65d30e1875372bf177c6162ba6194e9e5d57240a
eziraa <ezratgab@gmail.com>
2024-02-19 16:14:33 +0300

    creating admin class for Tag model

--- tags/models.py
+++ tags/models.py
@@ -18,6 +18,9 @@ class TeggedItemManager(models.Manager):
 class Tag(models.Model):
     label = models.CharField(max_length=255)
 
+    def __str__(self) -> str:
+        return self.label
+
 # create TeggedItem model
 
 
