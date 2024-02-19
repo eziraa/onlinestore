c39dd976913631253870bb8f3d521d8964566375
eziraa <ezratgab@gmail.com>
2024-02-19 14:15:35 +0300

    making the data validation to leave out promotions and description field to can be null

--- store/models.py
+++ store/models.py
@@ -18,13 +18,14 @@ class Collection(models.Model):
 # create Product model
 class Product(models.Model):
     title = models.CharField(max_length=255)
-    description = models.TextField()
+    description = models.TextField(null=True, blank=True)
     slug = models.SlugField(default='-')
     unit_price = models.DecimalField(max_digits=6, decimal_places=2)
     inventory = models.IntegerField()
     last_update = models.DateTimeField(auto_now=True)
     collection = models.ForeignKey(Collection, on_delete=models.PROTECT)
-    promotions = models.ManyToManyField(Promotion, related_name='products')
+    promotions = models.ManyToManyField(
+        Promotion, related_name='products', null=True, blank=True)
 
     def __str__(self):
         return self.title
