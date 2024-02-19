1a245e10b6852ecf62400a7f090e523bfcf018fa
eziraa <ezratgab@gmail.com>
2024-02-19 12:41:24 +0300

    Addin cutom Inventory class for product list apge

--- store/admin.py
+++ store/admin.py
@@ -3,12 +3,34 @@ from django.contrib import admin
 from django.db.models.aggregates import Count
 from django.db.models.query import QuerySet
 from django.http import HttpRequest
+from django.db.models import Q
 from django.urls import reverse
 from django.utils.html import format_html, urlencode
 from .models import Collection, Order, OrderItem, Customer, Product, Promotion
 list_of_models = [Order, OrderItem, Promotion]
 admin.site.register(list_of_models)
 
+# Creating Custom Inventory filter
+
+
+class InventoryFilter(admin.SimpleListFilter):
+    title = 'Inventory'
+    parameter_name = 'inventory'
+
+    def lookups(self, request: Any, model_admin: Any):
+        return [
+            ('<10', 'Low'),
+            ('else', 'Ok'),
+
+        ]
+
+    def queryset(self, request: Any, queryset: QuerySet[Any]) -> QuerySet[Any] | None:
+        if self.value() == '<10':
+            return queryset.filter(inventory__lt=10)
+        elif self.value() == 'else':
+            return queryset.filter(Q(inventory__gte=10))
+
+        return queryset
 
 @admin.register(Collection)
 class CollectionAdmin(admin.ModelAdmin):
@@ -32,8 +54,10 @@ class ProductAdmin(admin.ModelAdmin):
     list_display = ['title', 'unit_price', 'inventory_status', 'collection']
     list_editable = ['unit_price']
     list_per_page = 10
+    list_filter = ['collection', 'last_update', InventoryFilter]
     ordering = ['inventory']
-    def inventory_status(self, product):
+
+    def inventory_status(self, product: Product):
         if product.inventory < 10:
             return 'Low'
         return 'Ok'
@@ -45,9 +69,10 @@ class CustomerAdmin(admin.ModelAdmin):
     list_editable = ['membership']
     list_per_page = 15
     ordering = ['first_name', 'last_name']
-
+    search_fields = ['first_name__istartswith', 'last_name__istartswith']
     def orders(self, customer):
         return customer.orders
 
     def get_queryset(self, request: HttpRequest) -> QuerySet[Any]:
         return super().get_queryset(request).annotate(orders=Count('order'))
+
