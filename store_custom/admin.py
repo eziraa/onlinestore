53a526be1ff3722cfa48a23a0547a2094e099ce7
eziraa <ezratgab@gmail.com>
2024-02-19 22:44:40 +0300

    Implementing relationship between Tag and product

--- store_custom/admin.py
+++ store_custom/admin.py
@@ -1,1 +1,21 @@
 from django.contrib import admin
+
+from django.contrib.contenttypes.admin import GenericTabularInline
+from store.admin import ProductAdmin
+from store.models import Product
+from tags.models import TaggeItem
+
+
+class TaggedInline(GenericTabularInline):
+    autocomplete_fields = ['tag']
+    model = TaggeItem
+
+
+class CustomProductAdmin(ProductAdmin):
+    inlines = [TaggedInline]
+
+
+admin.site.unregister(Product)
+admin.site.register(Product, CustomProductAdmin)
+
+# Register your models here.
